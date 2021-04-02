722571f06c0911d5d05664b8e2b05195a376d697
kumquat <13495-x-KuOrang@users.noreply.gitlab.inria.fr>
2021-04-02 13:08:27 +0200

    ajout install chrome gitlab-ci

--- CLEA-lib/js/karma.conf.js
+++ CLEA-lib/js/karma.conf.js
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['FirefoxHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['FirefoxHeadless','ChromeHeadlessNoSandbox'],
     customLaunchers: {
       FirefoxHeadless: {
         base: 'Firefox',
